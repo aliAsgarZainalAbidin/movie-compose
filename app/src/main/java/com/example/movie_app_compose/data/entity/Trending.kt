c3bff298229fd0166e70a217716d1a13545b9cbd
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2021-08-19 11:13:59 +0800

    update abstact class Movie

--- app/src/main/java/com/example/movie_app_compose/data/entity/Trending.kt
+++ app/src/main/java/com/example/movie_app_compose/data/entity/Trending.kt
@@ -10,31 +10,31 @@ import com.google.gson.annotations.SerializedName
 data class Trending(
     @ColumnInfo(name = "release_date")
     @field:SerializedName("release_date")
-    override var releaseDate: String = "",
+    override var releaseDate: String? = "",
 
     @ColumnInfo(name = "adult")
     @field:SerializedName("adult")
-    var adult: Boolean? = false,
+    override var adult: Boolean? = false,
 
     @ColumnInfo(name = "backdrop_path")
     @field:SerializedName("backdrop_path")
-    var backdropPath: String? = "",
+    override var backdropPath: String? = "",
 
     @ColumnInfo(name = "genre_ids")
     @field:SerializedName("genre_ids")
-    var genreIds: List<Int>? = listOf(),
+    override var genreIds: List<Int>? = listOf(),
 
     @ColumnInfo(name = "vote_count")
     @field:SerializedName("vote_count")
-    var voteCounts: Int? = -1,
+    override var voteCounts: Int? = -1,
 
     @ColumnInfo(name = "original_language")
     @field:SerializedName("original_language")
-    var originalLanguage: String? = "",
+    override var originalLanguage: String? = "",
 
     @ColumnInfo(name = "original_title")
     @field:SerializedName("original_title")
-    var originalTitle: String? = "",
+    override var originalTitle: String? = "",
 
     @ColumnInfo(name = "poster_path")
     @field:SerializedName("poster_path")
@@ -42,30 +42,30 @@ data class Trending(
 
     @ColumnInfo(name = "video")
     @field:SerializedName("video")
-    var video: Boolean? = false,
+    override var video: Boolean? = false,
 
     @PrimaryKey
     @ColumnInfo(name = "id")
     @field:SerializedName("id")
-    var id: Int? = -1,
+    override var id: Int? = -1,
 
     @ColumnInfo(name = "vote_average")
     @field:SerializedName("vote_average")
-    override var voteAvarage: Float = 0.0f,
+    override var voteAvarage: Float? = 0.0f,
 
     @ColumnInfo(name = "title")
     @field:SerializedName("title")
-    override var title: String = "",
+    override var title: String? = "",
 
     @ColumnInfo(name = "overview")
     @field:SerializedName("overview")
-    var overview: String? = "",
+    override var overview: String? = "",
 
     @ColumnInfo(name = "popularity")
     @field:SerializedName("popularity")
-    var popularity: Double? = 0.0,
+    override var popularity: Double? = 0.0,
 
     @ColumnInfo(name = "mediaType")
     @field:SerializedName("media_type")
-    var mediaType: String? = ""
+    override var mediaType: String? = ""
 ) : Movie()
--- app/src/main/java/com/example/movie_app_compose/util/Movie.kt
+++ app/src/main/java/com/example/movie_app_compose/util/Movie.kt
@@ -1,58 +1,71 @@
 package com.example.movie_app_compose.util
 
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 
 abstract class Movie {
-    abstract val voteAvarage: Float?
-    abstract val releaseDate: String?
-    abstract val title: String?
-    abstract val posterPath: String?
+    abstract var releaseDate: String?
+    abstract var adult: Boolean?
+    abstract var backdropPath: String?
+    abstract var genreIds: List<Int>?
+    abstract var voteCounts: Int?
+    abstract var originalLanguage: String?
+    abstract var originalTitle: String?
+    abstract var posterPath: String
+    abstract var video: Boolean?
+    abstract var id: Int?
+    abstract var voteAvarage: Float?
+    abstract var title: String?
+    abstract var overview: String?
+    abstract var popularity: Double?
+    abstract var mediaType: String?
 }
 
 //    (
 //    @field:SerializedName("release_date")
-//    open var releaseDate: String? = "",
+//    open abstract var releaseDate: String? = "",
 //
 //    @field:SerializedName("adult")
-//    open var adult: Boolean? = false,
+//    open abstract var adult: Boolean? = false,
 //
 //    @field:SerializedName("backdrop_path")
-//    open var backdropPath: String? = "",
+//    open abstract var backdropPath: String? = "",
 //
 //    @field:SerializedName("genre_ids")
-//    open var genreIds: List<Int>? = listOf(),
+//    open abstract var genreIds: List<Int>? = listOf(),
 //
 //    @field:SerializedName("vote_count")
-//    open var voteCounts: Int? = -1,
+//    open abstract var voteCounts: Int? = -1,
 //
 //    @field:SerializedName("original_language")
-//    open var originalLanguage: String? = "",
+//    open abstract var originalLanguage: String? = "",
 //
 //    @field:SerializedName("original_title")
-//    open var originalTitle: String? = "",
+//    open abstract var originalTitle: String? = "",
 //
 //    @field:SerializedName("poster_path")
-//    open var posterPath: String? = "",
+//    open abstract var posterPath: String? = "",
 //
 //    @field:SerializedName("video")
-//    open var video: Boolean? = false,
+//    open abstract var video: Boolean? = false,
 //
 //    @field:SerializedName("id")
-//    open var id: Int? = -1,
+//    open abstract var id: Int? = -1,
 //
 //    @field:SerializedName("vote_average")
-//    open var voteAvarage: Double? = 0.0,
+//    open abstract var voteAabstract varage: Double? = 0.0,
 //
 //    @field:SerializedName("title")
-//    open var title: String? = "",
+//    open abstract var title: String? = "",
 //
 //    @field:SerializedName("overview")
-//    open var overview: String? = "",
+//    open abstract var overview: String? = "",
 //
 //    @field:SerializedName("popularity")
-//    open var popularity: Double? = 0.0,
+//    open abstract var popularity: Double? = 0.0,
 //
 //    @field:SerializedName("media_type")
-//    open var mediaType: String? = ""
+//    open abstract var mediaType: String? = ""
 //)
