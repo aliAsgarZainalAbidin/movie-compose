a9d9881f823918ec067a9228fa8d680a9d039bc6
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2021-08-23 08:53:39 +0800

    add loading overview UI

--- app/src/main/java/com/example/movie_app_compose/ui/overview/Overview.kt
+++ app/src/main/java/com/example/movie_app_compose/ui/overview/Overview.kt
@@ -1,5 +1,8 @@
 package com.example.movie_app_compose.ui.overview
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.ScrollableDefaults
@@ -26,6 +29,7 @@ import com.example.movie_app_compose.BuildConfig
 import com.example.movie_app_compose.api.ApiFactory
 import com.example.movie_app_compose.data.AppDatabase
 import com.example.movie_app_compose.data.Repository
+import com.example.movie_app_compose.ui.IdleContent
 import com.example.movie_app_compose.ui.components.LazyRowCommonItem
 import com.example.movie_app_compose.ui.components.LazyRowItem
 import com.example.movie_app_compose.ui.components.LazyRowLandscapeItem
@@ -49,156 +53,160 @@ fun OverviewBody(
     var listTrending = overviewViewModel.getTrendingMovies().observeAsState()
     var listOnTheAir = overviewViewModel.getOnTheAir().observeAsState()
 
-    Column(
-        modifier = modifier
-            .verticalScroll(
-                scrollState,
-                flingBehavior = ScrollableDefaults.flingBehavior()
-            )
-            .fillMaxHeight()
-    ) {
-        ConstraintLayout(
+    if (listPeople.value != null && listTrending.value != null && listOnTheAir.value != null){
+        Column(
             modifier = modifier
-                .fillMaxWidth()
+                .verticalScroll(
+                    scrollState,
+                    flingBehavior = ScrollableDefaults.flingBehavior()
+                )
                 .fillMaxHeight()
         ) {
-            val (tvWelcome, lzRow, tvPopular, lzRowPopular, tvMovies, lzRowMovies, spacer) = createRefs()
-            Text(
-                buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Green500
-                        )
-                    ) {
-                        append("Trending")
-                    }
-                },
-                modifier = modifier.constrainAs(tvWelcome) {
-                    top.linkTo(lzRowPopular.bottom, 16.dp)
-                    start.linkTo(parent.start, 16.dp)
-                    width = Dimension.preferredWrapContent
-                }
-            )
-
-            LazyRow(
-                modifier = modifier.constrainAs(lzRow) {
-                    top.linkTo(tvWelcome.bottom, 8.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                contentPadding = PaddingValues(horizontal = 16.dp)
+            ConstraintLayout(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
             ) {
-                items(listTrending.value?.size ?: 0) { index ->
-                    val data = listTrending.value?.get(index)
-                    val title = data?.title ?: ""
-                    val imageUrl = data?.posterPath ?: ""
-                    val date = data?.releaseDate ?: ""
-                    val voteAverage = data?.voteAverage ?: 0f
-                    LazyRowItem(
-                        modifier = modifier.clickable {
-                            onItemClickListener(Const.TYPE_MOVIE, data?.id.toString())
-                        },
-                        imageUrl = imageUrl,
-                        title = title,
-                        date = date,
-                        voteAverage = voteAverage
-                    )
-                }
-            }
+                val (tvWelcome, lzRow, tvPopular, lzRowPopular, tvMovies, lzRowMovies, spacer) = createRefs()
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Green500
+                            )
+                        ) {
+                            append("Trending")
+                        }
+                    },
+                    modifier = modifier.constrainAs(tvWelcome) {
+                        top.linkTo(lzRowPopular.bottom, 16.dp)
+                        start.linkTo(parent.start, 16.dp)
+                        width = Dimension.preferredWrapContent
+                    }
+                )
 
-            Text(
-                buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Green500
+                LazyRow(
+                    modifier = modifier.constrainAs(lzRow) {
+                        top.linkTo(tvWelcome.bottom, 8.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp)
+                ) {
+                    items(listTrending.value?.size ?: 0) { index ->
+                        val data = listTrending.value?.get(index)
+                        val title = data?.title ?: ""
+                        val imageUrl = data?.posterPath ?: ""
+                        val date = data?.releaseDate ?: ""
+                        val voteAverage = data?.voteAverage ?: 0f
+                        LazyRowItem(
+                            modifier = modifier.clickable {
+                                onItemClickListener(Const.TYPE_MOVIE, data?.id.toString())
+                            },
+                            imageUrl = imageUrl,
+                            title = title,
+                            date = date,
+                            voteAverage = voteAverage
                         )
-                    ) {
-                        append("Popular People\n")
-                    }
-                    withStyle(style = SpanStyle(fontSize = 11.sp, color = Green600)) {
-                        append("Millions of movies, Tv Shows and people to discover")
-                    }
-                },
-                modifier = modifier.constrainAs(tvPopular) {
-                    top.linkTo(parent.top, 16.dp)
-                    start.linkTo(parent.start, 16.dp)
-                    width = Dimension.preferredWrapContent
-                }
-            )
-
-            LazyRow(
-                modifier = modifier.constrainAs(lzRowPopular) {
-                    top.linkTo(tvPopular.bottom, 8.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                contentPadding = PaddingValues(horizontal = 16.dp)
-            ) {
-                items(listPeople.value?.size ?: 0) { index ->
-                    listPeople.value?.let {
-                        LazyRowPopularItem(people = it.get(index = index))
                     }
                 }
-            }
 
-            Text(
-                buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Green500
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Green500
+                            )
+                        ) {
+                            append("Popular People\n")
+                        }
+                        withStyle(style = SpanStyle(fontSize = 11.sp, color = Green600)) {
+                            append("Millions of movies, Tv Shows and people to discover")
+                        }
+                    },
+                    modifier = modifier.constrainAs(tvPopular) {
+                        top.linkTo(parent.top, 16.dp)
+                        start.linkTo(parent.start, 16.dp)
+                        width = Dimension.preferredWrapContent
+                    }
+                )
+
+                LazyRow(
+                    modifier = modifier.constrainAs(lzRowPopular) {
+                        top.linkTo(tvPopular.bottom, 8.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp)
+                ) {
+                    items(listPeople.value?.size ?: 0) { index ->
+                        listPeople.value?.let {
+                            LazyRowPopularItem(people = it.get(index = index))
+                        }
+                    }
+                }
+
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Green500
+                            )
+                        ) {
+                            append("On The Air")
+                        }
+                    },
+                    modifier = modifier.constrainAs(tvMovies) {
+                        top.linkTo(lzRow.bottom, 16.dp)
+                        start.linkTo(parent.start, 16.dp)
+                        width = Dimension.preferredWrapContent
+                    }
+                )
+
+                LazyRow(
+                    modifier = modifier.constrainAs(lzRowMovies) {
+                        top.linkTo(tvMovies.bottom, 8.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp)
+                ) {
+                    items(listOnTheAir.value?.size ?: 0) { index ->
+                        val data = listOnTheAir.value?.get(index)
+                        val title = data?.name ?: ""
+                        val imageUrl = data?.backdropPath ?: ""
+                        val date = data?.firstAirDate ?: ""
+                        val voteAverage = data?.voteAverage ?: 0f
+                        LazyRowLandscapeItem(
+                            modifier = modifier.clickable {
+                                onItemClickListener(Const.TYPE_TV, data?.id.toString())
+                            },
+                            imageUrl = imageUrl,
+                            title = title,
+                            date = date,
+                            voteAverage = voteAverage
                         )
-                    ) {
-                        append("On The Air")
                     }
-                },
-                modifier = modifier.constrainAs(tvMovies) {
-                    top.linkTo(lzRow.bottom, 16.dp)
-                    start.linkTo(parent.start, 16.dp)
-                    width = Dimension.preferredWrapContent
                 }
-            )
 
-            LazyRow(
-                modifier = modifier.constrainAs(lzRowMovies) {
-                    top.linkTo(tvMovies.bottom, 8.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                contentPadding = PaddingValues(horizontal = 16.dp)
-            ) {
-                items(listOnTheAir.value?.size ?: 0) { index ->
-                    val data = listOnTheAir.value?.get(index)
-                    val title = data?.name ?: ""
-                    val imageUrl = data?.backdropPath ?: ""
-                    val date = data?.firstAirDate ?: ""
-                    val voteAverage = data?.voteAverage ?: 0f
-                    LazyRowLandscapeItem(
-                        modifier = modifier.clickable {
-                            onItemClickListener(Const.TYPE_TV, data?.id.toString())
-                        },
-                        imageUrl = imageUrl,
-                        title = title,
-                        date = date,
-                        voteAverage = voteAverage
-                    )
-                }
+                Spacer(modifier = modifier
+                    .width(16.dp)
+                    .constrainAs(spacer) {
+                        top.linkTo(lzRowMovies.bottom, margin = 16.dp)
+                    })
             }
-
-            Spacer(modifier = modifier
-                .width(16.dp)
-                .constrainAs(spacer) {
-                    top.linkTo(lzRowMovies.bottom, margin = 16.dp)
-                })
         }
+    } else {
+        IdleContent()
     }
 }
 
